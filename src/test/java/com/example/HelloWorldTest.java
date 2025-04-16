80086ffafec66c8a7833f3a072ac4ec6a1aa8f52
zylena <study.zylena@gmail.com>
2025-04-16 15:52:00 +0800

    second commit

--- src/test/java/com/example/HelloWorldTest.java
+++ src/test/java/com/example/HelloWorldTest.java
@@ -6,6 +6,6 @@ public class HelloWorldTest {
     @Test
     public void testGetGreeting() {
         HelloWorld helloWorld = new HelloWorld();
-        assertEquals("Hello, World!", helloWorld.getGreeting());
+        assertEquals("Hello, WorldXXX!", helloWorld.getGreeting());
     }
 }
